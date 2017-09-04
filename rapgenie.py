0e6b1102df89d7a4c635a2d375d3f34b2e35b75c
Ben Pankow <pankow.ben@gmail.com>
2017-09-04 13:43:05 -0500

    Added genius object and search functionality

--- rapgenie.py
+++ rapgenie.py
@@ -11,6 +11,7 @@ API_GENIUS_URL = 'https://api.genius.com/'
 
 API_SONG_BASE_URL = API_GENIUS_URL + 'songs/'
 API_ARTISTS_BASE_URL = API_GENIUS_URL + 'artists/'
+API_SEARCH_BASE_URL = API_GENIUS_URL + 'search?q='
 
 # Searches for a number of terms in a given string
 # Returns a tuple with the first element the index of the first locate
@@ -37,11 +38,11 @@ def get_song_data(song):
         song_id = id_base[id_base.rfind('/') + 1:]
         song.song_id = song_id
         # Fetch API info
-        json_response = _api_access(API_SONG_BASE_URL + song.song_id)
+        json_response = song.genie.api_access(API_SONG_BASE_URL + song.song_id)
 
     # Fetch song API info, extract song url from page
     elif song.song_id:
-        json_response = _api_access(API_SONG_BASE_URL + song.song_id)
+        json_response = song.genie.api_access(API_SONG_BASE_URL + song.song_id)
         song.url = json_response['response']['song']['url']
         # Fetch song page
         html_response = _bs_spoof(song.url)
@@ -53,24 +54,24 @@ def get_song_data(song):
         json_data = json_response['response']['song']
         song.title = json_data['title']
         song.release_date = json_data['release_date']
-        song.artist = Artist.from_id(json_data['primary_artist']['id'])
+        song.artist = song.genie.artist_from_id(json_data['primary_artist']['id'])
         song.artist.name = json_data['primary_artist']['name']
         song.featured_artists = []
 
         for featured_artist in json_data['featured_artists']:
-            featured_artist_obj = Artist.from_id(featured_artist['id'])
+            featured_artist_obj = song.genie.artist_from_id(featured_artist['id'])
             featured_artist_obj.name = featured_artist['name']
             song.featured_artists.append(featured_artist_obj)
 
-        song.additional_vocals = []
+        song.credits = {}
 
         for additional_credits in json_data['custom_performances']:
-            if 'Additional Vocals' in additional_credits['label']:
-                for vocal_artist in additional_credits['artists']:
-                    featured_artist_obj = Artist.from_id(vocal_artist['id'])
-                    featured_artist_obj.name = vocal_artist['name']
-                    print featured_artist_obj.name
-                    song.additional_vocals.append(featured_artist_obj)
+            label = additional_credits['label']
+            song.credits[label] = []
+            for credited_artist in additional_credits['artists']:
+                credited_artist_obj = song.genie.artist_from_id(credited_artist['id'])
+                credited_artist_obj.name = credited_artist['name']
+                song.credits[label].append(credited_artist_obj)
 
         # Store song lyrics, in plaintext and html
         lyrics_html = html_response('div', {'class' : 'lyrics'})[0]
@@ -101,10 +102,16 @@ def process_song_fragments(song):
     current_section = Section('Intro', song.artist)
     current_artist = song.artist
     current_fragment_tags = []
-    tags_to_look_for = ['[', '<i>', '</i>', '<b>', '</b>', '<em>', '</em>']
+    tags_to_look_for_base = ['[', '<i>', '</i>', '<b>', '</b>', '<em>', '</em>', '<strong>', '</strong>']
+    tags_to_look_for = tags_to_look_for_base[:]
 
     # Search for section header or HTML tag
     found_index, found_type = _min_search(lyrics_left, tags_to_look_for)
+
+    # Potential artists who may deliver lyrics in the song
+    potential_artists = song.featured_artists + [song.artist]
+    if 'Additional Vocals' in song.credits:
+        potential_artists += song.credits['Additional Vocals']
 
     while found_type:
         # Create fragment of text up to the found tag / header
@@ -158,7 +165,7 @@ def process_song_fragments(song):
                 if artist_bs.html and artist_bs.html.body:
                     # Locate HTML tags around an artist's name, to signify what
                     # their lyrics will be tagged with
-                    tags = artist_bs.html.body.findAll(['b', 'i', 'em'])
+                    tags = artist_bs.html.body.findAll(['b', 'i', 'em', 'strong'])
                     artist_name = artist_bs.text
                     has_parens = False
 
@@ -174,7 +181,8 @@ def process_song_fragments(song):
 
                     artist_obj = None
                     max_ratio = 0
-                    for featured_artist in song.featured_artists + [song.artist]:
+
+                    for featured_artist in potential_artists:
                         ratio = difflib.SequenceMatcher(None, featured_artist.name, artist_name).ratio()
                         if ratio > .8 and ratio > max_ratio:
                             artist_obj = featured_artist
@@ -190,10 +198,9 @@ def process_song_fragments(song):
             # If an artist in this section is identified by parenthesis, make
             # sure to search for them in the lyrics, otherwise treat them as
             # ordinary text
+            tags_to_look_for = tags_to_look_for_base[:]
             if look_for_parens:
-                tags_to_look_for = ['[', '<i>', '</i>', '<b>', '</b>', '<em>', '</em>', '(', ')']
-            else:
-                tags_to_look_for = ['[', '<i>', '</i>', '<b>', '</b>', '<em>', '</em>']
+                tags_to_look_for += ['(', ')']
             current_section = Section(tag_name, [section_artists['']] + section_artists.values())
 
             lyrics_left = lyrics_left[end_bracket_index + 1:]
@@ -213,8 +220,6 @@ def process_song_fragments(song):
             if ''.join(current_fragment_tags) in section_artists:
                 current_artist = section_artists[''.join(current_fragment_tags)]
             else:
-                for artist in section_artists:
-                    print '> ', artist
                 current_artist = None
             found_index, found_type = _min_search(lyrics_left, tags_to_look_for)
 
@@ -237,11 +242,6 @@ def _bs_spoof(url):
     response = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
     return BeautifulSoup(response.text, 'lxml')
 
-# Access the specific API url and return its JSON result
-def _api_access(url):
-    response = requests.get(url, headers = {'Authorization': 'Bearer ' + CLIENT_ACCESS_TOKEN, 'User-Agent': 'Mozilla/5.0'})
-    return response.json()
-
 # A fragment is a line or series of lines associated with an artist
 # Contains an artist string (TODO object) and a small amount of text
 class Fragment:
@@ -260,23 +260,12 @@ class Section:
 # Unfinished, an artist class will eventually hold more info about an artist
 # TODO
 class Artist:
-    @staticmethod
-    def from_url(url):
-        artist = Artist()
-        artist.url = url
-        return artist
-
-    @staticmethod
-    def from_id(artist_id):
-        artist = Artist()
-        artist.artist_id = str(artist_id)
-        return artist
-
-    def __init__(self):
+    def __init__(self, genie):
         self.url = None
         self.artist_id = None
         self.has_data = False
         self.name = None
+        self.genie = genie
 
     def __str__(self):
         if self.has_data:
@@ -294,24 +283,16 @@ class Artist:
 # Can be created by ID or url, but does not contain information until data is
 # requested from the api using song_obj.request()
 class Song:
-    @staticmethod
-    def from_url(url):
-        song = Song()
-        song.url = url
-        return song
-
-    @staticmethod
-    def from_id(song_id):
-        song = Song()
-        song.song_id = str(song_id)
-        return song
-
-    def __init__(self):
+    def __init__(self, genie):
         self.url = None
         self.song_id = None
         self.has_data = False
         self.has_fragments = False
         self.sections = None
+        self.featured_artists = None
+        self.artist = None
+        self.credits = None
+        self.genie = genie
 
     def request(self):
         if not self.has_data:
@@ -331,14 +312,54 @@ class Song:
         else:
             return 'Unrequested song with ID ' + self.song_id
 
+class Genie:
 
-new_god_flow = Song.from_url('https://genius.com/Travis-scott-90210-lyrics')
-new_god_flow.request().parse()
+    def __init__(self, client_access_token):
+        self.client_access_token = client_access_token
+
+    def song_from_url(self, song_url):
+        song = Song(self)
+        song.url = song_url
+        return song
+
+    def song_from_id(self, song_id):
+        song = Song(self)
+        song.song_id = str(song_id)
+        return song
+
+    def artist_from_url(self, artist_url):
+        artist = Artist(self)
+        artist.url = artist_url
+        return artist
+
+    def artist_from_id(self, artist_id):
+        artist = Artist(self)
+        artist.artist_id = str(artist_id)
+        return artist
+
+    def search(self, query):
+        response =  self.api_access(API_SEARCH_BASE_URL + query)['response']['hits']
+        for song in response:
+            song_obj = self.song_from_id(song['result']['id'])
+            song_obj.title = song['result']['title']
+            yield song_obj
+
+    # Access the specific API url and return its JSON result
+    def api_access(self, url):
+        response = requests.get(url, headers = {'Authorization': 'Bearer ' + self.client_access_token, 'User-Agent': 'Mozilla/5.0'})
+        return response.json()
+
+genie = Genie(CLIENT_ACCESS_TOKEN)
+for result in genie.search('telephone calls'):
+    for artist in result.request().featured_artists:
+        print artist.name
+song = genie.song_from_url('https://genius.com/A-ap-ferg-mad-man-lyrics')
+song.request().parse()
 
 output = ''
-for section in new_god_flow.sections:
+for section in song.sections:
     for fragment in section.fragments:
-        name = 'None'
+        name = 'Other'
         if fragment.artist:
             name = fragment.artist.name
         output += name + '\n' + fragment.text.replace('\n', ' ') + '\n\n'
@@ -346,6 +367,7 @@ for section in new_god_flow.sections:
 print 'writ'
 with open('test.txt', 'w+') as f:
     f.write(output.encode('utf-8'))
+
 
 '''
 songs = [
